d808f7c7f3d0ed8a86554910d9f47a694f9b4d6a
aakash <asoni1940@gmail.com>
2022-10-10 00:45:43 +0530

    node attach

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
 import mongoose, { Mongoose } from "mongoose"
+import dotenv from "dotenv";
+
+dotenv.config()
 
   const MoviesDB=()=>{
     // mongoose.connect("mongodb://localhost:27017/moviesapp2022")
-    mongoose.connect("mongodb+srv://aakash:as1997@movies-cluster.lnkqfix.mongodb.net/test")
+    mongoose.connect(`${process.env.MONGO_URL}`)
     .then(()=>{
         console.log("Server is connected to mongoDB atlas")
     })
