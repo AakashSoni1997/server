fa501a333e3b1e7137c6e49c9b632fefacb4594a
aakash <asoni1940@gmail.com>
2023-01-08 15:29:46 +0530

    version3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import mongoose, { Mongoose } from "mongoose"
+import dotenv from "dotenv";
 import express from "express";
 // import { Router } from "express";
-import MoviesDB from "./config/database";
+// import MoviesDB from "./config/database";
 // import movieRouter from "./routes/Movie-route";
 // import router from "./routes/user-routes.js";
 import cors from "cors";
@@ -14,7 +16,6 @@ app.use(express.json());
 // app.use("/api/user", router);
 // app.use("/api/movie", movieRouter);
 // dotenv.config()
-MoviesDB();
 
 app.get("/honey",(req,res)=>{
   res.send("hello world")
@@ -31,5 +32,25 @@ app.get("/honey",(req,res)=>{
 
 
 // app.listen(PORT, () => {
-//   console.log("this server is running on port 5000");
-// });
+  //   console.log("this server is running on port 5000");
+  // });
+  
+  
+  dotenv.config()
+
+  const MoviesDB=()=>{
+    //  mongoose.connect("mongodb://localhost:27017/moviesapp2022")
+    mongoose.connect(`mongodb+srv://aakash:as1997@movies-cluster.lnkqfix.mongodb.net/test`)
+    .then(()=>{
+        console.log("Server is connected to mongoDB atlas")
+    })
+    .catch((err)=>{
+      console.log(err.message)
+    })
+    
+}
+
+
+
+
+MoviesDB();
